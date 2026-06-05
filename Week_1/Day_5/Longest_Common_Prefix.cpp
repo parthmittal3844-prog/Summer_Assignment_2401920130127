c8da239f55630e1087a8a6baeca1f49f201a3b38
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-05 19:12:27 +0530

    Update Longest_Common_Prefix.cpp

--- Week_1/Day_5/Longest_Common_Prefix.cpp
+++ Week_1/Day_5/Longest_Common_Prefix.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        int n=strs.size();
+        string s1=strs[0];
+        string s2=strs[n-1];
+        int mini=min(s1.size(),s2.size());
+        int ans=0;
+        for(int i=0;i<mini;i++){
+        if(s1[i]==s2[i]){
+            ans++;
+        }
+        else{
+            break;
+        }
+        }
+            return s1.substr(0,ans);
+        
+    }
+};
