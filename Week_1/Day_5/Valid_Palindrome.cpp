a6ecb24e418ca6b44c550f922afd36499dffdf0a
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-05 19:05:28 +0530

    Update Valid_Palindrome.cpp

--- Week_1/Day_5/Valid_Palindrome.cpp
+++ Week_1/Day_5/Valid_Palindrome.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string temp="";
+        for(char ch:s){
+            if(isalnum(ch)){
+                temp+=tolower(ch);
+            }
+        }
+        int n=temp.size();
+        int l=0;
+        int r=n-1;
+        while(l<r){
+            if(temp[l]!=temp[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
