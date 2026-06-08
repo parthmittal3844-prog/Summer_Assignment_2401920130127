e39b3b2f0fba46132ae0aa15e6f398203ba36d17
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-08 08:15:12 +0530

    Update and rename Valid_Anagram to Valid_Anagram.cpp

---
+++ Week_2/Day_1/Valid_Anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        if(n!=m){
+            return false;
+        }
+        int freq[26]={0};
+        for(int i=0;i<n;i++){
+            freq[s[i]-'a']++;
+        }
+        for(int i=0;i<m;i++){
+            freq[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
