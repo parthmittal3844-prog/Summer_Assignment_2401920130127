7b1bea9a95031a0eb18ab6cda3fc2881bf1ae224
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-08 08:36:17 +0530

    Create Ransom_Note.cpp

---
+++ Week_2/Day_1/Ransom_Note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int n=ransomNote.size();
+        int m=magazine.size();
+        if(m<n){
+            return false;
+        }
+        int freq[26]={0};
+        for(int i=0;i<n;i++){
+            freq[ransomNote[i]-'a']++;
+        }
+        for(int i=0;i<m;i++){
+            freq[magazine[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
