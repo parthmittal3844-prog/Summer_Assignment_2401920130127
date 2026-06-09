0fa8c2c56c13a89d0e7537abeb32b768f722d78f
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-09 09:50:42 +0530

    Update Find_All_Anagrams_in_a_String.cpp

--- Week_2/Day_2/Find_All_Anagrams_in_a_String.cpp
+++ Week_2/Day_2/Find_All_Anagrams_in_a_String.cpp
@@ -1,1 +1,28 @@
-
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        int n=s.size();
+        int m=p.size();
+        vector<int>ans;
+        if(m>n){
+            return ans;
+        }
+        vector<int>hash(26,0);
+        vector<int>freq(26,0);
+        for(int i=0;i<m;i++){
+            hash[p[i]-'a']++;
+            freq[s[i]-'a']++;
+        }
+        if(hash==freq){
+            ans.push_back(0);
+        }
+        for(int i=m;i<n;i++){
+            freq[s[i]-'a']++;
+            freq[s[i-m]-'a']--;
+            if(hash==freq){
+                ans.push_back(i-m+1);
+            }
+        }
+        return ans;
+    }
+};
