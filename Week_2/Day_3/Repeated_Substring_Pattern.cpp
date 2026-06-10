bff827419acc52d64508fd83fa872fcde8e93a68
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-10 08:39:30 +0530

    Update Repeated_Substring_Pattern.cpp

--- Week_2/Day_3/Repeated_Substring_Pattern.cpp
+++ Week_2/Day_3/Repeated_Substring_Pattern.cpp
@@ -1,1 +1,9 @@
-
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string t=s+s;
+        int n=t.size();
+        string sub=t.substr(1,n-2);
+        return sub.find(s)!=-1;
+    }
+};
