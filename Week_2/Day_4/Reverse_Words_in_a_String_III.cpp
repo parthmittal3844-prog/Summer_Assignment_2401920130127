80512d0ce96024cc1516e65f40c3af83df666d1f
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-11 08:12:31 +0530

    Update Reverse_Words_in_a_String_III.cpp

--- Week_2/Day_4/Reverse_Words_in_a_String_III.cpp
+++ Week_2/Day_4/Reverse_Words_in_a_String_III.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.size();
+        string ans = "";
+        int i = 0;
 
+        while(i < n) {
+             string word = "";
+
+            while(i < n && s[i] != ' ') {
+                word += s[i];
+                i++;
+            }
+            reverse(word.begin(), word.end());
+            ans += word;
+            if(s[i] == ' ') {
+                ans += ' ';
+                i++;
+               
+            }
+        }
+        return ans;
+    }
+};
