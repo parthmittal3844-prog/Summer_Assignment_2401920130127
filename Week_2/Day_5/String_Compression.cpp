6305d006374de2cbfddd4f8ac178262fbfc1c9c3
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-12 01:38:26 +0530

    Update String_Compression.cpp

--- Week_2/Day_5/String_Compression.cpp
+++ Week_2/Day_5/String_Compression.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int n=chars.size();
+        int i=0;
+        int index=0;
+        while(i<n){
+            char c=chars[i];
+            int cnt=0;
+            while(i<n && chars[i]==c){
+                cnt++;
+                i++;
+            }
+            chars[index]=c;
+            index++;
+            if(cnt>1){
+                string num=to_string(cnt);
+                for(char n: num){
+                    chars[index]=n;
+                    index++;
+                }
+            }
+        }
+        return index;
+    }
+};
