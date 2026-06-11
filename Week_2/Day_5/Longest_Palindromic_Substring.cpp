d3e74a4f9865d3ea35d918197492ce9792604fba
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-12 01:41:57 +0530

    Update Longest_Palindromic_Substring.cpp

--- Week_2/Day_5/Longest_Palindromic_Substring.cpp
+++ Week_2/Day_5/Longest_Palindromic_Substring.cpp
@@ -1,1 +1,33 @@
-
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n=s.size();
+        int start=0;
+        int maxlen=1;
+        for(int i=0;i<n;i++){
+            int l=i;
+            int r=i;
+            while(l>=0 && r<n && s[l]==s[r]){
+                int len=r-l+1;
+                if(len>maxlen){
+                    maxlen=len;
+                    start=l;
+                }
+                l--;
+                r++;
+            }
+            l=i;
+            r=i+1;
+            while(l>=0 && r<n && s[l]==s[r]){
+                int len=r-l+1;
+                if(len>maxlen){
+                    maxlen=len;
+                    start=l;
+                }
+                l--;
+                r++;
+            }
+        }
+        return s.substr(start,maxlen);
+    }
+};
