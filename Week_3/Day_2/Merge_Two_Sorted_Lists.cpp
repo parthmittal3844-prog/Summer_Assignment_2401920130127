242efac2741e9606f58c025244a294e40b33bb66
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-16 10:56:25 +0530

    Update Merge_Two_Sorted_Lists.cpp

--- Week_3/Day_2/Merge_Two_Sorted_Lists.cpp
+++ Week_3/Day_2/Merge_Two_Sorted_Lists.cpp
@@ -1,1 +1,32 @@
-
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* t1=list1;
+        ListNode* t2=list2;
+        ListNode* dummyNode= new ListNode(-1);
+        ListNode* temp=dummyNode;
+        while(t1!=NULL && t2!=NULL){
+            if(t1->val < t2->val){
+                temp->next=t1;
+                temp=t1;
+                t1=t1->next;
+            }
+            else{
+                temp->next=t2;
+                temp=t2;
+                t2=t2->next;
+            }
+        }
+        while(t1!=NULL){
+            temp->next=t1;
+            temp=t1;
+            t1=t1->next;
+        }
+        while(t2!=NULL){
+            temp->next=t2;
+            temp=t2;
+            t2=t2->next;
+        }
+        return dummyNode->next;
+    }
+};
