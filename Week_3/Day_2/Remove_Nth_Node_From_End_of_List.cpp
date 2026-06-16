722bca318a3aa48e4e44cfac174953eee32544ee
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-16 12:43:48 +0530

    Update Remove_Nth_Node_From_End_of_List.cpp

--- Week_3/Day_2/Remove_Nth_Node_From_End_of_List.cpp
+++ Week_3/Day_2/Remove_Nth_Node_From_End_of_List.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int cnt=0;
+        ListNode* temp=head;
+        while(temp!=NULL){
+            cnt++;
+            temp=temp->next;
+        }
+        if(cnt==n){
+            return head->next;
+        }
+        int result=cnt-n;
+        temp=head;
+        while(temp!=NULL){
+            result--;
+            if(result==0){
+                break;
+            }
+            temp=temp->next;
+        }
+        ListNode* deleteNode=temp->next;
+        temp->next=temp->next->next;
+        delete(deleteNode);
+        return head;
+    }
+};
