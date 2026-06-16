e1c22665295859b1608804e2fa90b8dbdd323296
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-16 18:26:35 +0530

    Update Test.java

--- Week_2/OOPS/Test.java
+++ Week_2/OOPS/Test.java
@@ -1,1 +1,18 @@
+package live;
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test{
+    public static void main(String[] args){
+        Veena v= new Veena();
+        v.play();
+        Saxophone s= new Saxophone();
+        s.play();
+        Playable p=new Veena();
+        p.play();
+        Playable p1=new Saxophone();
+        p1.play();
+    }
+}
 
