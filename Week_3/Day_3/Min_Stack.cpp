90188f1ea0de36a754df667e448e88b02a559864
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-17 17:23:20 +0530

    Update Min_Stack.cpp

--- Week_3/Day_3/Min_Stack.cpp
+++ Week_3/Day_3/Min_Stack.cpp
@@ -1,1 +1,29 @@
+class MinStack {
+public:
 
+    stack<pair<int,int>>st;
+    MinStack() {
+        
+    }
+    
+    void push(int value) {
+        if(st.empty()){
+            st.push({value,value});
+        }
+        else{
+            st.push({value,min(value,st.top().second)});
+        }
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
