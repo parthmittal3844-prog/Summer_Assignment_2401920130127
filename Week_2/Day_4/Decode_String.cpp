4a52995443b721bb1d832739b8c9f490febbcbc2
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-18 17:28:28 +0530

    Update Decode_String.cpp

--- Week_2/Day_4/Decode_String.cpp
+++ Week_2/Day_4/Decode_String.cpp
@@ -1,1 +1,36 @@
-
+class Solution {
+public:
+    string decodeString(string s) {
+        int n=s.size();
+        stack<int>num;
+        stack<string>strs;
+        string result="";
+        int k=0;
+        for(int i=0;i<n;i++){
+            if(isdigit(s[i])){
+                k=k*10+(s[i]-'0');
+            }
+            else if(s[i]=='['){
+                num.push(k);
+                strs.push(result);
+                k=0;
+                result="";
+            }
+            else if(s[i]==']'){
+                string a=strs.top();
+                int b=num.top();
+                strs.pop();
+                num.pop();
+                string temp="";
+                for(int j=0;j<b;j++){
+                    temp+=result;
+                }
+                result=a+temp;
+            }
+            else{
+                result+=s[i];
+            }
+        }
+        return result;
+    }
+};
