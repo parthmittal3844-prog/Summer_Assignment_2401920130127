285aada1543d2799af35edb7de2839e297272182
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-18 17:27:47 +0530

    Update Generate_Parentheses.cpp

--- Week_2/Day_4/Generate_Parentheses.cpp
+++ Week_2/Day_4/Generate_Parentheses.cpp
@@ -1,1 +1,23 @@
+class Solution {
+public:
 
+     vector<string>ans;
+
+    void create(string s, int open, int close, int n){
+        if(s.size()==2*n){
+            ans.push_back(s);
+            return;
+        }
+        if(open<n){
+            create(s+"(",open+1,close,n);
+        }
+        if(close<open){
+            create(s+")",open,close+1,n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+       create("",0,0,n);
+       return ans;
+    }
+};
