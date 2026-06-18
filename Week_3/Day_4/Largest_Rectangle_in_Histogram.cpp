76a6d621a4c7afc3c562cb662dfabf1810a61ceb
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-18 13:33:54 +0530

    Update Largest_Rectangle_in_Histogram.cpp

--- Week_3/Day_4/Largest_Rectangle_in_Histogram.cpp
+++ Week_3/Day_4/Largest_Rectangle_in_Histogram.cpp
@@ -1,1 +1,55 @@
+class Solution {
+public:
 
+    vector<int> findNse(vector<int>heights){
+        int n=heights.size();
+        vector<int>nse(n);
+        stack<int>st;
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
+                st.pop();
+            }
+            if(st.empty()){
+               nse[i]=n;
+            }
+            else{
+                nse[i]=st.top();
+            }
+            st.push(i);
+        }
+        return nse;
+    }
+
+    vector<int> findPse(vector<int>heights){
+        int n=heights.size();
+        stack<int>st;
+        vector<int>pse(n);
+        for(int i=0;i<n;i++){
+            while(!st.empty() && heights[st.top()]>=heights[i]){
+                st.pop();
+            }
+            if(st.empty()){
+               pse[i]=-1;
+            }
+            else{
+                pse[i]=st.top();
+            }
+            st.push(i);
+        }
+        return pse;
+    }
+
+    int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+        vector<int>nse(n);
+        vector<int>pse(n);
+        int maxi=0;
+        nse=findNse(heights);
+        pse=findPse(heights);
+
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,heights[i]*(nse[i]-pse[i]-1));
+        }
+        return maxi;
+    }
+};
