a0948dd092e7413fa0b52f17b6401fe2a3a5c580
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-22 11:02:50 +0530

    Update box3d.java

--- Week_4/OOPS/Question_4/box3d.java
+++ Week_4/OOPS/Question_4/box3d.java
@@ -1,1 +1,11 @@
-
+class box3d extends box{
+    double height;
+    
+    box3d(double length, double breadth, double height){
+        super(length,breadth);
+        this.height=height;
+    }
+    double volume(){
+        return length*breadth*height;
+    }
+}
