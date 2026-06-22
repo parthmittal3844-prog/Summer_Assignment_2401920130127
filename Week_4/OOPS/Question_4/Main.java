93127b53c2e0a1f4be0f82e2c1a26448f3bdbbe9
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-22 11:03:17 +0530

    Update Main.java

--- Week_4/OOPS/Question_4/Main.java
+++ Week_4/OOPS/Question_4/Main.java
@@ -1,1 +1,9 @@
-
+public class Main{
+    public static void main(String[] args){
+        box b=new box(1,2);
+        System.out.println(b.area());
+        
+        box3d b1=new box3d(1,2,3);
+        System.out.println(b1.volume());
+    }
+}
