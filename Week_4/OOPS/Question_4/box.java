c585c9a39426b612f274e249828922505d685188
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-22 11:02:14 +0530

    Update box.java

--- Week_4/OOPS/Question_4/box.java
+++ Week_4/OOPS/Question_4/box.java
@@ -1,1 +1,12 @@
-
+class box{
+    double length;
+    double breadth;
+    
+    box(double length, double breadth){
+        this.length=length;
+        this.breadth=breadth;
+    }
+    double area(){
+        return length*breadth;
+    }
+}
