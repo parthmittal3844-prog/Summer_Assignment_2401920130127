cc309d609275f4a17c7ed5c88a2ee7718dbf426f
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-22 11:00:20 +0530

    Update Main.java

--- Week_4/OOPS/Question_3/Main.java
+++ Week_4/OOPS/Question_3/Main.java
@@ -1,1 +1,11 @@
+public class Main{
+    public static void main(String[] args){
+        Point p1= new Point();
+        p1.display();
 
+        Point p2= new Point(10, 20);
+        p2.display();
+        p2.setXY(30, 40);
+        p2.display();
+    }
+}
