bade0aeea5c92125415fb34c76398a613d33f2c3
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-22 10:58:02 +0530

    Update Main.java

--- Week_4/OOPS/Question_2/Main.java
+++ Week_4/OOPS/Question_2/Main.java
@@ -1,1 +1,21 @@
+class Outer{
+    void display(){
+        System.out.println("Outer class is displayed");
+    }
 
+    class Inner{
+        void display(){
+            System.out.println("Inner class is displayed");
+        }
+    }
+}
+
+public class Main{
+    public static void main(String[] args){
+        Outer o=new Outer();
+        o.display();
+
+        Outer.Inner i=o.new Inner();
+        i.display();
+    }
+}
