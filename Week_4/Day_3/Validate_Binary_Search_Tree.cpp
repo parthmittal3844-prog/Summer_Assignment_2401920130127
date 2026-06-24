5f054a5f4f46f06c30ecd34a4fbac00e93b585d5
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-24 14:08:07 +0530

    Update Validate_Binary_Search_Tree.cpp

--- Week_4/Day_3/Validate_Binary_Search_Tree.cpp
+++ Week_4/Day_3/Validate_Binary_Search_Tree.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
 
+    bool isValid(TreeNode* root, long minVal, long maxVal){
+        if(root==NULL){
+            return true;
+        }
+        if(root->val >= maxVal || root->val <= minVal){
+            return false;
+        }
+        return isValid(root->left,minVal,root->val) && isValid(root->right,root->val,maxVal);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return isValid(root,LONG_MIN,LONG_MAX);
+    }
+};
