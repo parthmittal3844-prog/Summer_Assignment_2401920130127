a94677afb4e9ef58586116d935c534c9f164c28a
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-25 18:37:53 +0530

    Update Path_Sum.cpp

--- Week_4/Day_4/Path_Sum.cpp
+++ Week_4/Day_4/Path_Sum.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root==NULL){
+            return false;
+        }
+        targetSum=targetSum-root->val;
+        if(root->left==NULL && root->right==NULL){
+            if(targetSum==0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        return hasPathSum(root->left,targetSum) || hasPathSum(root->right,targetSum);
+    }
+};
