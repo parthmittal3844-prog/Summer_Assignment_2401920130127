16cc0174715260fad0b5503cf5937af6daeff356
parthmittal3844-prog <parthmittal3844@gmail.com>
2026-06-25 18:37:35 +0530

    Update Diameter_of_Binary_Tree.cpp

--- Week_4/Day_4/Diameter_of_Binary_Tree.cpp
+++ Week_4/Day_4/Diameter_of_Binary_Tree.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
 
+    int height(TreeNode* root, int &maxi){
+        if(root==NULL){
+            return 0;
+        }
+        int leftHeight=height(root->left,maxi);
+        int rightHeight=height(root->right,maxi);
+        maxi=max(maxi,leftHeight+rightHeight);
+        return 1+max(leftHeight,rightHeight);
+    }
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxi=0;
+        height(root,maxi);
+        return maxi;
+    }
+};
